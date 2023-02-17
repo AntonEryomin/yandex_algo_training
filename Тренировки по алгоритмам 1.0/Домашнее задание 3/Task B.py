6c7516fe37ec934561fdc36300f4eed3f09b7749
antoneremin <antonerjomin@gmail.com>
2023-02-17 13:37:05 +0300

    Поправлена задача

--- Тренировки по алгоритмам 1.0/Домашнее задание 3/Task B.py
+++ Тренировки по алгоритмам 1.0/Домашнее задание 3/Task B.py
@@ -1,5 +1,19 @@
-# Считываем ввод
-nums_1 = set(input().split(" "))
-nums_2 = set(input().split(" "))
+from typing import List
 
-print(" ".join(sorted(list(nums_1.intersection(nums_2)))))
+
+def solution(nums_1: List[int], nums_2: List[int]) -> List[int]:
+    nums_1 = set(nums_1)
+    nums_2 = set(nums_2)
+    # Получаем список чисел, входящих в оба множества в отсортированном виде
+    sorted_common_nums = sorted(list(nums_1.intersection(nums_2)))
+
+    return " ".join([str(_) for _ in sorted_common_nums])
+
+
+if __name__ == "__main__":
+    # Считываем ввод
+    nums_2 = [int(_) for _ in input().split(" ")]
+    nums_1 = [int(_) for _ in input().split(" ")]
+
+    # Выводим ответ
+    print(solution(nums_1, nums_2))
